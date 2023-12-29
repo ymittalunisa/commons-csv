9a29262ae62369677736591e2723bb3b46990616
ymittalunisa <y.mittal@studenti.unisa.it>
2023-12-29 20:36:04 +0100

    updated ExtendedBufferedReader.java

--- src/main/java/org/apache/commons/csv/ExtendedBufferedReader.java
+++ src/main/java/org/apache/commons/csv/ExtendedBufferedReader.java
@@ -174,24 +174,28 @@ final class ExtendedBufferedReader extends BufferedReader {
     
         final int len = super.read(buf, offset, length);
     
-        int eolCount = 0;
-
+        
         if (len > 0) {
+
             for (int i = offset; i < offset + len; i++) {
                 final char ch = buf[i];
-                if (ch == CR) {
-                    if (i < offset + len - 1 && buf[i + 1] == LF) {
-                        i++;
+                if (ch == LF) {
+                    if (CR != (i > offset ? buf[i - 1] : lastChar)) {
+                        eolCounter++;
                     }
-                } else if (ch == LF) {
-                    eolCount++;
+                } else if (ch == CR) {
+                    eolCounter++;
                 }
             }
+
+            lastChar = buf[offset + len - 1];
+
         } else if (len == -1) {
             lastChar = END_OF_STREAM;
-        } else {
-            throw new IOException("Unexpected read length");
         }
+
+        position += len;
+        return len;
         eolCounter += eolCount;
         
         position += len;
